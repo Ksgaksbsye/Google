933075d7d775ca044a6c110c3b985955b028c422
Ksgaksbsye <naincyprajapati122008@gmail.com>
2025-03-26 23:22:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://api.nexgenbots.in') #youtube song url
+API_KEY = getenv("API_KEY", None) # youtube song api key, get it from https://t.me/VISHNUSONI14
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Ksgaksbsye/Google",
